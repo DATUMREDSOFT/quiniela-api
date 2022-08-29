4a914bfab18002a72d1ed1d16f31c5afe297dae1
guzmanespinoza <luisespinoza.le109@gmail.com>
2022-08-28 23:56:35 -0600

    Timeout

--- src/app/models/quinielaModel.js
+++ src/app/models/quinielaModel.js
@@ -72,18 +72,23 @@ Quiniela.getJuego = (req, result) => {
 
 
             for (let d of res[2]) {
-                obtenerEncuentros(d.id).then(resp => {
-                    fases.push({ ...d, partidos: resp })
+                let query2 = "SELECT * FROM partidos where idFase = ?";
+
+                sql.query(query2, [d.id], (err2, res2) => {
+                    console.log(res2)
+                    fases.push({ ...d, partidos: res2 });
                 });
+
             }
 
-
+           setTimeout(() => {
             let response = {
                 participante: res[0][0],
                 quiniela: { ...res[1][0], fases: fases },
             }
 
             result(null, { state: 'success', data: response, message: null });
+           }, 5000);
         }
         else {
             result(null, { state: 'fail', data: res, message: null });
@@ -93,18 +98,7 @@ Quiniela.getJuego = (req, result) => {
 };
 
 
-const obtenerEncuentros = (idFase) => {
-    return new Promise((resolve, reject) => {
-        let query = "SELECT * FROM partidos where idFase = ?";
-        sql.query(query, [idFase], (error, results, fields) => {
-            if (error) return reject(error);
-            console.log('Consulta correcta');
-            return resolve(results);
-        });
 
-    });
-
-}
 
 
 module.exports = Quiniela;
